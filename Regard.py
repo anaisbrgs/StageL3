7be380df473f1439314971251d8aa05e358d4464
nicolasCambon <cambonnicolas@yahoo.fr>
2018-03-21 17:59:09 +0100

    modif du code regard

--- Regard.py
+++ Regard.py
@@ -28,23 +28,13 @@ def show(img, gamma=.5, noise_level=.4, transpose=True):
     else:
         plt.imshow(npimg)
 
-image_data = torchvision.datasets.ImageFolder('datasetN',
-transforms.Compose([transforms.CenterCrop(150),
-transforms.ToTensor()]))
-
-data_loader=torch.utils.data.DataLoader(image_data, batch_size=256,
-shuffle=True)
-
-dataiter = iter(data_loader)
-images,foo = next(dataiter)
-
 #show(torchvision.utils.make_grid(images, normalize=True))
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+parser.add_argument('--batch-size', type=int, default=110, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+parser.add_argument('--test-batch-size', type=int, default=25, metavar='N',
                     help='input batch size for testing (default: 1000)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
@@ -72,15 +62,14 @@ if args.cuda:
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
-train_loader = torch.utils.data.DataLoader(image_data, batch_size=200,
-shuffle=True)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=args.test_batch_size, shuffle=True, **kwargs)
+train_loader = torch.utils.data.DataLoader(torchvision.datasets.ImageFolder('datasetN/Train',
+transforms.Compose([transforms.CenterCrop(150),
+transforms.ToTensor()])), batch_size=args.batch_size,shuffle=True)
 
+test_loader = torch.utils.data.DataLoader(torchvision.datasets.ImageFolder('datasetN/Test',
+transforms.Compose([transforms.CenterCrop(150),
+transforms.ToTensor()])), batch_size=args.test_batch_size,shuffle=True)
+                                          
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
