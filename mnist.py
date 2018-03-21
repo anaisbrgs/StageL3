a8d4e006784809ef2ae55e97b0f694dde63168fa
nicolasCambon <cambonnicolas@yahoo.fr>
2018-03-21 16:30:42 +0100

    revert to previous sha

--- mnist.py
+++ mnist.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import argparse
+import time
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -25,15 +27,20 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
+parser.add_argument('--dimension', type=int, default = 50, metavar='D',
+                    help='the dimension of the second neuron network') #ajout de l'argument dimension représentant le nombre de neurone dans la deuxième couche. 
+parser.add_argument('--boucle', type=int, default=0, metavar='B',
+                   help='boucle pour faire différents couche de la deuxième couche de neurone')# ajout de boucle pour automatiser le nombre de neurone dans la deuxieme couche
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
-
+print ('cuda?', args.cuda)
 torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=True, download=True,
                    transform=transforms.Compose([
@@ -48,15 +55,14 @@ test_loader = torch.utils.data.DataLoader(
                    ])),
     batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(320, args.dimension)# args.dimension prend la valeur default de l'argument dimension.
+        self.fc2 = nn.Linear(args.dimension, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
@@ -84,10 +90,11 @@ def train(epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+        if args.log_interval>0: # rajout de la commande pour pouvoir print ou non les différents epoch ou juste le résultat
+            if batch_idx % args.log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.data[0]))
 
 def test():
     model.eval()
@@ -103,11 +110,35 @@ def test():
         correct += pred.eq(target.data.view_as(pred)).long().cpu().sum()
 
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    if args.log_interval>0: print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+    return correct / len(test_loader.dataset)
 
+def protocol():
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+    return test()
 
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test()
+def main():
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        Accuracy = test()
+    print('Test set: Final Accuracy: {:.3f}%'.format(Accuracy*100)) # print que le pourcentage de réussite final
+    
+    
+if __name__ == '__main__':  
+    if args.boucle == 1: # Pour que la boucle se fasse indiquer --boucle 1
+        rho = 10**(1/3) 
+        for i in [int (k) for k in rho**np.arange(2,9)]:# i prend les valeur en entier du tuple rho correspondra au nombre de neurone
+            args.dimension = i
+            print ('La deuxième couche de neurone comporte',i,'neurones')
+            main()
+    else:
+        t0 = time.time () # ajout de la constante de temps t0
+
+        main()
+
+        t1 = time.time () # ajout de la constante de temps t1
+
+        print ("Le programme a mis",t1-t0, "secondes à s'exécuter.") #compare t1 et t0, connaitre le temps d'execution du programme
