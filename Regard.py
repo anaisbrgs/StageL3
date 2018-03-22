334e4d5a9d0773b3fe33ae93d2fd0fedd4962aa4
nicolasCambon <cambonnicolas@yahoo.fr>
2018-03-22 16:43:19 +0100

    try to change2

--- Regard.py
+++ Regard.py
@@ -30,7 +30,7 @@ parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
-parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M', #default = 0.5
                     help='SGD momentum (default: 0.5)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
@@ -38,7 +38,7 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
-parser.add_argument('--dimension', type=int, default = 50, metavar='D',
+parser.add_argument('--dimension', type=int, default = 120, metavar='D',
                     help='the dimension of the second neuron network') #ajout de l'argument dimension représentant le nombre de neurone dans la deuxième couche. 
 parser.add_argument('--boucle', type=int, default=0, metavar='B',
                    help='boucle pour faire différents couche de la deuxième couche de neurone')# ajout de boucle pour automatiser le nombre de neurone dans la deuxieme couche
@@ -51,55 +51,61 @@ if args.cuda:
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
                        
-train_loader = torch.utils.data.DataLoader(torchvision.datasets.ImageFolder('dataset',train=True,
-                                            transform = transforms.Compose([
+train_loader = torch.utils.data.DataLoader(torchvision.datasets.ImageFolder('dataset',
+                                            transforms.Compose([
                                             transforms.CenterCrop(150),
                                             transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                                            ])), batch_size=args.batch_size, shuffle=True)
+                                           transforms.Normalize((0.1307,), (0.3081,))
+                                            ])), batch_size=args.batch_size, shuffle=True,num_workers=2 )
 
 test_loader= torch.utils.data.DataLoader(torchvision.datasets.ImageFolder                                                                 
-                                             ('dataset',train=False,
-                                            transform = transforms.Compose([
+                                             ('dataset',
+                                            transforms.Compose([
                                             transforms.CenterCrop(150),
                                             transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                                            ])), batch_size=args.test_batch_size, shuffle=True)
-                                          
+                                           transforms.Normalize((0.1307,), (0.3081,))
+                                            ])), batch_size=args.test_batch_size, shuffle=True,num_workers=2)
+classes = 'blink','left','right','center'
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.pool = nn.MaxPool2d(2, 2)#
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(12800, args.dimension)# args.dimension prend la valeur default de l'argument dimension.
-        self.fc2 = nn.Linear(args.dimension, 10)
-
+        self.fc2 = nn.Linear(args.dimension, 84)
+        self.fc3 = nn.Linear(84, 10)
+        
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 4))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 4))
         x = x.view(-1, 12800)
         x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))#
         x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        x = self.fc3(x)#
+        #x = self.fc2(x)
+        return F.log_softmax(x, dim=1)#x
 
 model = Net()
 if args.cuda:
     model.cuda()
 
+criterion = nn.CrossEntropyLoss()#
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
 def train(epoch):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+    for batch_idx, (data, target) in enumerate(train_loader,0):
         if args.cuda:
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data), Variable(target)
         optimizer.zero_grad()
         output = model(data)
         #loss = F.nll_loss(output, target)
-        loss = F.mse_loss(output, target)
+        loss = criterion(output, labels)#
         loss.backward()
         optimizer.step()
         if args.log_interval>0: # rajout de la commande pour pouvoir print ou non les différents epoch ou juste le résultat
@@ -107,6 +113,9 @@ def train(epoch):
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(data), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader), loss.data[0]))
+
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
 
 def test():
     model.eval()
